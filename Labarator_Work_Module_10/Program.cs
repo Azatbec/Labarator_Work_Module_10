c4dc5522ea40b54051fbf34a165a039f1a0d4cd6
Asyl Azatbec <azatbec2003@mail.ru>
2024-11-04 20:24:48 +0600

    Add project files.

---
+++ Labarator_Work_Module_10/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labarator_Work_Module_10
+{
+    public class AudioSystem
+    {
+        public void TurnOn()
+        {
+            Console.WriteLine("Аудио система включена ");
+        }
+        public void SetVolume(int level)
+        {
+            Console.WriteLine($"Уровень аудиро системы {level}");
+        }
+        public void TurnOff()
+        {
+            Console.WriteLine("Аудио система выключена ");
+        }
+
+    }
+
+    public class VideoProjector
+    {
+        public void TurnOn()
+        {
+            Console.WriteLine("Видео проектор включен");
+        }
+        public void SetResolution(string resolution)
+        {
+            Console.WriteLine($"Результат видео проекта {resolution}");
+        }
+        public void TurnOff()
+        {
+            Console.WriteLine("Видео проектор выключен");
+        }
+    }
+
+    public class LightingSystem
+    {
+        public void TurnOn()
+        {
+            Console.WriteLine("Яркость включена ");
+        }
+        public void SetBrightness(int level)
+        {
+            Console.WriteLine($"Уровень яркости{level}");
+        }
+        public void TurnOff()
+        {
+            Console.WriteLine("Яркость отключена");
+        }
+    }
+
+    public class HomeTheaterFacade
+    {
+        private AudioSystem audioSystem;
+        private VideoProjector videoProjector;
+        private LightingSystem lightingSystem;
+
+        public HomeTheaterFacade(AudioSystem audioSystem, VideoProjector videoProjector, LightingSystem lightingSystem)
+        {
+            this.audioSystem = audioSystem;
+            this.videoProjector = videoProjector;
+            this.lightingSystem = lightingSystem;
+        }
+
+        public void StartMove()
+        {
+            Console.WriteLine("Подготовка к началу фильма");
+            lightingSystem.TurnOn();
+            lightingSystem.SetBrightness(5);
+            audioSystem.TurnOn();
+            audioSystem.SetVolume(8);
+            videoProjector.TurnOn();
+            videoProjector.SetResolution("HD");
+            Console.WriteLine("Начала фильма");
+        }
+
+        public void EndMove()
+        {
+            Console.WriteLine("Выключение фильма");
+            videoProjector.TurnOff();
+            audioSystem.TurnOff();
+            lightingSystem.TurnOff();
+            Console.WriteLine("Конец фильма ");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args) 
+        { 
+            AudioSystem audioSystem = new AudioSystem();
+            VideoProjector videoProjector = new VideoProjector();
+            LightingSystem lightingSystem = new LightingSystem();
+
+            HomeTheaterFacade homeTheater = new HomeTheaterFacade(audioSystem, videoProjector, lightingSystem);
+            homeTheater.StartMove();
+
+            Console.WriteLine();
+            homeTheater.EndMove();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Labarator_Work_Module_10_2/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Labarator_Work_Module_10_2
+{
+    internal class Program
+    {
+        public abstract class FileSystemComponent
+        {
+            protected string name;
+
+            public FileSystemComponent(string name)
+            {
+                this.name = name;
+            }
+
+            public abstract void Display(int depth);
+
+            public virtual void Add(FileSystemComponent component)
+            {
+                throw new NotImplementedException();
+            }
+            public virtual void Remove(FileSystemComponent component)
+            {
+                throw new NotImplementedException();
+            }
+            public virtual FileSystemComponent GetChild(int index)
+            { 
+                throw new NotImplementedException ();
+            }
+
+            
+        }
+
+        public class File : FileSystemComponent
+        {
+            public File(string name) : base(name)
+            { 
+                
+            }
+            public override void Display(int depth)
+            {
+                Console.WriteLine(new string('_', depth) + "File:" + name);
+            }
+        }
+        public class Directory : FileSystemComponent
+        {
+            private List<FileSystemComponent> children = new List<FileSystemComponent>();
+            public Directory(string name) : base(name) { }
+            public override void Add(FileSystemComponent component) 
+            { 
+                children.Add(component);
+            }
+            public override void Remove(FileSystemComponent component)
+            {
+                children.Remove(component);
+            }
+
+            public override FileSystemComponent GetChild(int index)
+            {
+                return base.GetChild(index);
+            }
+
+            public override void Display(int depth)
+            {
+                Console.WriteLine(new string('-', depth) + " Directory: " + _name);
+                foreach (var component in _children)
+                {
+                    component.Display(depth + 2);
+                }
+
+            }
+
+
+        }
+
+        class Prgram
+        {
+            static void Main(string[] args)
+            {
+                Directory root = new Directory("Root");
+                File file1 = new File("Fil1.text");
+                File file2 = new File("Fil2.text");
+
+                Directory subDir = new Directory("SubDirectory");
+                File subFile1 = new File("SubFile1.txt");
+
+                root.Add(file1);
+                root.Add(file2);
+                subDir.Add(subFile1);
+                root.Add(subDir);
+
+                root.Display(1);
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
